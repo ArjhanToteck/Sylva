ae650ea19debeebf04de70fdad1c440915bad23f
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2023-04-04 22:58:46 -0600

    Added case for both boundaries being visible in camera
    If both bounds of a given dimension are visible by the camera, the camera just stays in the middle of them

--- Assets/Scripts/Behaviours/CameraController.cs
+++ Assets/Scripts/Behaviours/CameraController.cs
@@ -24,6 +24,7 @@ public class CameraController : MonoBehaviour
 		// follows player
 		Vector3 targetPosition = player.position + offset;
 
+		// stay within boundaries
         if(xMin != null)
         {
             targetPosition.x = Mathf.Max(targetPosition.x, xMin.position.x + cameraHalfWidth);
@@ -42,6 +43,17 @@ public class CameraController : MonoBehaviour
 		if (yMax != null)
 		{
 			targetPosition.y = Mathf.Min(targetPosition.y, yMax.position.y - cameraHalfHeight);
+		}
+
+		// if both bounds of a given dimension are visible, stay between them
+		if(xMax != null && xMin != null && targetPosition.x + cameraHalfWidth >= xMax.position.x && targetPosition.x - cameraHalfWidth <= xMin.position.x)
+		{
+			targetPosition.x = (xMin.position.x + xMax.position.x) / 2f;
+		}
+
+		if (yMax != null && yMin != null && targetPosition.y + cameraHalfWidth >= yMax.position.y && targetPosition.y - cameraHalfWidth <= yMin.position.y)
+		{
+			targetPosition.y = (yMin.position.y + yMax.position.y) / 2f;
 		}
 
 		transform.position = targetPosition;
